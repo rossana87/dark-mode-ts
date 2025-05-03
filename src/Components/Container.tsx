10081f60264022b4a00d89fdce599ed6605e2f94
rossana87 <rossana.ventrella@gmail.com>
2025-05-03 10:55:28 +0100

    fix-removes react from imports

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import useTheme from "../assets/context/useTheme";
 import About from "./About";
 import Interests from "./Interests";
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import useTheme from "../assets/context/useTheme";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
